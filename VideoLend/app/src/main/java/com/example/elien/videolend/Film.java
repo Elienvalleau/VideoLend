36836d59efeda51bd70a51a03714168af127eb11
elien <elienvalleau@gmail.com>
2017-01-23 18:15:51 +0100

    classe film

---
+++ VideoLend/app/src/main/java/com/example/elien/videolend/Film.java
@@ -0,0 +1,16 @@
+package com.example.elien.videolend;
+
+import java.util.ArrayList;
+
+/**
+ * Created by utilisateur on 23/01/2017.
+ */
+
+public class Film {
+
+    private String titre;
+    private String realisateur;
+    private int annee;
+    private ArrayList<String> acteurs = new ArrayList<>();
+    private String synopsys;
+}
