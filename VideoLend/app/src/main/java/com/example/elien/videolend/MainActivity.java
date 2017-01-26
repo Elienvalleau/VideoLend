c34b431ce143a37070997905c1b5e80c32b6ff08
pierrebourges <pierre.bourges@ynov.com>
2017-01-26 19:21:30 +0100

    Image en parti

--- VideoLend/app/src/main/java/com/example/elien/videolend/MainActivity.java
+++ VideoLend/app/src/main/java/com/example/elien/videolend/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.elien.videolend;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +23,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        GridView gridView = (GridView) findViewById(R.id.gridview);
+        gridView.setAdapter(new ImageAdapter(this));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, ""+position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -27,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    LayoutInflater inflater = View.inflate();
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,4 +72,56 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public class ImageAdapter extends BaseAdapter{
+        private Context mContext;
+
+        public ImageAdapter(Context c){
+            mContext = c;
+        }
+
+        public int getCount(){
+            return mThumbIds.length;
+        }
+
+        public Object getItem(int position){
+            return null;
+        }
+
+        public long getItemId(int position){
+            return 0;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent){
+            ImageView imageView;
+            if (convertView == null){
+                imageView = new ImageView(mContext);
+                convertView.setTag(imageView);
+            }
+            else{
+                imageView = (ImageView) convertView.getTag();
+            }
+            imageView.setImageResource(mThumbIds[position]);
+            return imageView;
+        }
+
+        private  Integer[] mThumbIds = {
+
+                R.drawable.sample_0,
+                R.drawable.sample_1,
+                R.drawable.sample_3,
+                R.drawable.sample_4,
+                R.drawable.sample_5,
+                R.drawable.sample_6,
+                R.drawable.sample_7,
+                R.drawable.sample_0,
+                R.drawable.sample_1,
+                R.drawable.sample_3,
+                R.drawable.sample_4,
+                R.drawable.sample_5,
+                R.drawable.sample_6,
+                R.drawable.sample_7,
+        };
+    }
+
 }
